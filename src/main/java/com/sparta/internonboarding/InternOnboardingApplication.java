e3224ef06f6bed0862cda25fd377ae24de59adaf
inzulmi <inzulmi132@gmail.com>
2025-01-17 18:29:45 +0900

    git init

---
+++ src/main/java/com/sparta/internonboarding/InternOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.internonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternOnboardingApplication.class, args);
+    }
+
+}
