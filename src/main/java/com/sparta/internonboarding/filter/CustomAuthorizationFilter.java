c617e62681cb1fc8f4b5f0689712a6f5e5edd41f
inzulmi <inzulmi132@gmail.com>
2025-01-18 15:47:35 +0900

    [#12] feat: CustomAuthorizationFilter 구현

--- src/main/java/com/sparta/internonboarding/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/sparta/internonboarding/filter/CustomAuthorizationFilter.java
@@ -6,8 +6,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -20,7 +27,26 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     private final UserDetailsServiceImpl userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        doFilter(request, response, filterChain);
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String accessToken = jwtUtil.getJwtFromRequest(request);
+
+        if(StringUtils.hasText(accessToken)) {
+            jwtUtil.validateToken(accessToken);
+
+            String userId = jwtUtil.getSubjectFromToken(accessToken);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+            context.setAuthentication(authentication);
+            SecurityContextHolder.setContext(context);
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
