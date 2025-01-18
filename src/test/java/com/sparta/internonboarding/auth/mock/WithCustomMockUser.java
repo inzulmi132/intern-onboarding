279446c372ba2cc7d4951974162da07df16ffaa5
inzulmi <inzulmi132@gmail.com>
2025-01-19 01:39:30 +0900

    [#28] feat: WithCustomMockUser 어노테이션 구현

---
+++ src/test/java/com/sparta/internonboarding/auth/mock/WithCustomMockUser.java
@@ -0,0 +1,13 @@
+package com.sparta.internonboarding.auth.mock;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithCustomMockUserSecurityContextFactory.class)
+public @interface WithCustomMockUser {
+
+    String username();
+}
