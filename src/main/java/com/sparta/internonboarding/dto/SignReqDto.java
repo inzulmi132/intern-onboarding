8bafed2da663f833e409d07b9a069b76b52a39e9
inzulmi <inzulmi132@gmail.com>
2025-01-18 16:29:37 +0900

    [#13] feat: 필터 및 로그인 기능 구현

---
+++ src/main/java/com/sparta/internonboarding/dto/SignReqDto.java
@@ -0,0 +1,9 @@
+package com.sparta.internonboarding.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SignReqDto {
+    private String username;
+    private String password;
+}
--- src/main/java/com/sparta/internonboarding/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/sparta/internonboarding/filter/CustomAuthenticationFilter.java
@@ -1,7 +1,19 @@
 package com.sparta.internonboarding.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.internonboarding.dto.SignReqDto;
 import com.sparta.internonboarding.jwt.JwtUtil;
+import com.sparta.internonboarding.userdetails.UserDetailsImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -9,6 +21,35 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
     public CustomAuthenticationFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
-        this.setFilterProcessesUrl("/sign");
+        setFilterProcessesUrl("/sign");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            SignReqDto reqDto = new ObjectMapper().readValue(request.getInputStream(), SignReqDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            reqDto.getUsername(),
+                            reqDto.getPassword(),
+                            null
+                    )
+            );
+        } catch(IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) {
+        String userId = ((UserDetailsImpl) auth.getPrincipal()).getUsername();
+        String accessToken = jwtUtil.generateAccessToken(userId);
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, JwtUtil.BEARER_PREFIX + accessToken);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
     }
 }
